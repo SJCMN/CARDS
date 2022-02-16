e7da13d78f5e294807da7587bb9c70f3436a91e1
SJCMN <sam.clavette@gmail.com>
2022-02-16 14:54:54 -0600

    move images to assets folder, add posts object array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CARDS';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets',
+      username: '',
+      content: '',
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: '',
+      username: '',
+      content: '',
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: '',
+      username: '',
+      content: '',
+    },
+  ];
 }
