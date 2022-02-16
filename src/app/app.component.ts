9b77782a1b6b61fdc52a66297ca7d1169a81d39f
SJCMN <sam.clavette@gmail.com>
2022-02-16 17:16:07 -0600

    add css :host and to card template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // posts is a property
   posts = [
     {
       title: 'Neat Tree',
