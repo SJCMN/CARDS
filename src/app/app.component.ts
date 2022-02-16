ad2b47162c80fa356cc126f9c7476c31aff7857a
SJCMN <sam.clavette@gmail.com>
2022-02-16 16:50:11 -0600

    add ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,21 @@ export class AppComponent {
   posts = [
     {
       title: 'Neat Tree',
-      imageUrl: 'assets',
-      username: '',
-      content: '',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'treebag',
+      content: 'This tree is looking like its lonely on this hill',
     },
     {
-      title: 'Neat Tree',
-      imageUrl: '',
-      username: '',
-      content: '',
+      title: 'Bunny Hop',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'singleSpeed',
+      content: 'Catch that sweet air!',
     },
     {
-      title: 'Neat Tree',
-      imageUrl: '',
-      username: '',
-      content: '',
+      title: 'Magnificent Range',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'ba-layon',
+      content: 'El Capitan is the third peek on the right',
     },
   ];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  // input decorator
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() username = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
